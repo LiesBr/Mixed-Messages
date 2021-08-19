9f073d93d19eabd396a085f6a2669f1a19e90161
Lies Braakman-Hogenhuis <lies@mountain-it.nl>
2021-08-19 11:26:05 +0200

    git commit -m 'first commit'

---
+++ README.md
@@ -0,0 +1,10 @@
+#Mixed-Messages 
+
+## Background
+
+This small program was developed as a part of the codecademy full stack developer course. The objective was to make a random message generator. 
+My inspiration came from my 1,5 year old daughter, who knows about 20 words, and will combine them randomly to make "sentences". 
+
+## Technologies used
+JS mostly, very basic html and css.
+Also a practice in version control via terminal
---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+const randomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+
+const hannahWords = {
+    nouns: ['Beer', 'Papa', 'Die', 'Dat', 'Papf', 'Auto', 'Poepf', 'Huis'],
+    verbs: ['oh-oow!', 'boem!', 'auw!', 'nat!', '¯\_(ツ)_/¯', 'bah!', 'ja.']
+}
+
+
+const sentenceGenerator = obj => {
+    h1.innerText = `${obj.nouns[randomIndex(obj.nouns)]}, ${obj.verbs[randomIndex(obj.verbs)]}`
+}
+
+btn.addEventListener('click', sentenceGenerator(hannahWords));
+
+
